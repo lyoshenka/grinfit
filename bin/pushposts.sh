5e1672ec3102e2e14fe99b8caff229b431e16fbc
Alex Grintsvayg <alex@flickswitch.cc>
2012-05-01 22:04:35 -0400

    better push?

--- bin/pushposts.sh
+++ bin/pushposts.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../";
-LOCKFILE="last.lck"
-NOW=$(date +%s)
-TIMEOUT=30 # so we only push once every 30 seconds
-
-# keep in mind that this lock is not reliable. make sure that in incron, we use solo.pl
-
 cd "$DIR"
-
-echo "$(date +%c) $@" >> log
-
-#if [ ! -e "$LOCKFILE" ] || (("$NOW" - $(cat "$LOCKFILE") >= "$TIMEOUT")) ; then
-#  git add -A _posts/ && git commit -m "New post" && git push heroku >> log
-#  echo "$NOW" > "$LOCKFILE"
-#fi
+git add -A _posts/ && git commit -m "New post" && git push heroku
---
+++ bin/pushpostsauto.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../";
+#LOCKFILE="last.lck"
+#NOW=$(date +%s)
+#TIMEOUT=30 # so we only push once every 30 seconds
+
+# keep in mind that this lock is not reliable. make sure that in incron, we use solo.pl
+
+#cd "$DIR"
+
+#if [ ! -e "$LOCKFILE" ] || (("$NOW" - $(cat "$LOCKFILE") >= "$TIMEOUT")) ; then
+if [[ "$@" =~ \.md$ ]] ; then
+  $DIR/pushposts.sh
+#  echo "$NOW" > "$LOCKFILE"
+fi
