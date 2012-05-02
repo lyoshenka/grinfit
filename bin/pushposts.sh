ec3a3e2cca93a3e6afe0bb200c7bccf2d825b92c
Alex Grintsvayg <alex@flickswitch.cc>
2012-05-01 20:46:17 -0400

    working on getting incron to work

--- bin/pushposts.sh
+++ bin/pushposts.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../_posts";
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../";
+LOCKFILE="last.lck"
+NOW=$(date +%s)
+TIMEOUT=30 # so we only push once every 30 seconds
+
+# keep in mind that this lock is not reliable. make sure that in incron, we use solo.pl
+
 cd "$DIR"
-git add -A . && git commit -m "New post" && git push heroku 2>>../err >>../log
+
+echo "$(date +%c) $@" >> log
+
+#if [ ! -e "$LOCKFILE" ] || (("$NOW" - $(cat "$LOCKFILE") >= "$TIMEOUT")) ; then
+#  git add -A _posts/ && git commit -m "New post" && git push heroku >> log
+#  echo "$NOW" > "$LOCKFILE"
+#fi
