4d7b7515333827a6466e089ec6bdb5c2773fa6e7
Alex Grintsvayg <alex@flickswitch.cc>
2012-05-02 09:07:48 -0400

    even better

--- bin/autopush.sh
+++ bin/autopush.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
-#LOCKFILE="last.lck"
-#NOW=$(date +%s)
-#TIMEOUT=30 # so we only push once every 30 seconds
 
-# keep in mind that this lock is not reliable. make sure that in incron, we use solo.pl
-
-#cd "$DIR"
-
-#if [ ! -e "$LOCKFILE" ] || (("$NOW" - $(cat "$LOCKFILE") >= "$TIMEOUT")) ; then
-#if [[ "$@" =~ \.md$ ]] ; then
-  #/home/grin/bin/solo.pl -port=44009 -silent $DIR/pushposts.sh
-  $DIR/pushposts.sh
-#  echo "$NOW" > "$LOCKFILE"
-#fi
+if [[ "$@" =~ \.md$ ]] ; then
+  # Only run pushposts if its not already running
+  $DIR/bin/solo.pl -port=44009 -silent $DIR/pushposts.sh
+fi
