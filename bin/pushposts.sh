0ee6f854586f9526687d54efcc7772493aefab17
Alex Grintsvayg <alex@flickswitch.cc>
2012-05-03 08:06:06 -0400

    New post

--- bin/pushposts.sh
+++ bin/pushposts.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..";
 cd "$DIR"
-git add -A _posts/ && git commit -m "New post" && git push && git push heroku
+git add -A _posts/ && git commit -m "New post" && git push && git push heroku master
