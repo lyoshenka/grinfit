4436436ab3ba9abf6e4c7f36c9ac3aac9d3ccd7b
Alex Grintsvayg <alex@flickswitch.cc>
2012-07-20 20:23:47 -0400

    added search

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+grinfit = typeof(grinfit) != "undefined" ? grinfit : {};
+grinfit.lastSearchTerm = '';
+
 $(document).ready(function() {
   var width = $(window).width(),
       smallWidth = 600;
@@ -6,30 +9,63 @@ $(document).ready(function() {
     $('.js-start-hidden').hide();
   }
 
-  $('.js-collapse').on('click', function() {
+  $('#container').on('click', '.js-collapse', function() {
     $(this).closest('article').find('.content').toggle();
   });
 
 
-/*
-  var token = '4fac62463f61b05dcf000088';
-  $.getJSON('http://tapirgo.com/api/1/search.json?token=' + token + '&query=' + paramValue('query') + '&callback=?', function(data){
-    var target = $('#posts');
-    target.html('');
-    $.each(data, function(key, val) {
-      target.append(
-        '<article>' +
-        '<a class="date clearfix" href="' + val.link + '">' + val.published_on + '</a>' +
-        '<h1 class="js-collapse">' + val.title + '</h1>' +
-        '<div>' +  val.content + '</div></article>'
-      );
-    });
+  doSearch = function(searchTerm) {
+    $('#search-progress').show();
+    if (!grinfit.searchCache) {
+      setTimeout(doSearch,200);
+    }
+
+    var term = $.trim(searchTerm.toLowerCase()),
+        regex = new RegExp(term, 'im');
+        results = []
+        i = 0;
+
+    if (term) {
+      if (term != grinfit.lastSearchTerm) {
+        grinfit.lastSearchTerm = term;
+        for (i in grinfit.searchCache) {
+          var post = grinfit.searchCache[i];
+          if (regex.test(post.title) || regex.test(post.contentClean)) {
+            results.push(post);
+          }
+        }
+
+        $('#posts').hide();
+        $('#search-results').html('').show();
+        for (i in results) {
+          var post = results[i];
+          $('#search-results').append(post.content + '<hr/>'
+//            '<article><a class="date clearfix" href="' + post.url + '">' + post.date + '</a>' + 
+//            '<h1><a class="js-collapse">' + post.title + '</a></h1>' + 
+//            '<div class="content js-start-hidden">' + post.content + '</div>' +
+//            '</article><hr/>'
+          );
+        }
+        $('#search-results .js-start-hidden').hide();
+      }
+    }
+    else {
+      $('#search-results').hide();
+      $('#posts').show();
+    }
+    $('#search-progress').hide();
+  };
+
+  $('#search').keyup(function() {
+    clearTimeout(grinfit.searchTimer);
+    var val = $(this).val();
+    grinfit.searchTimer = setTimeout(function() {
+      doSearch(val);
+    }, 200);
   });
 
-  // Extract the param value from the URL.
-  function paramValue(query_param) {
-    var results = new RegExp('[\\?&]' + query_param + '=([^&#]*)').exec(window.location.href);
-    return results ? results[1] : false;
-  }
-*/
+  $.get('/search.json', function(data) {
+    grinfit.searchCache = $.parseJSON(data);
+  });
+
 });
