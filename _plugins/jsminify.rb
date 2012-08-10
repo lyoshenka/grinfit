cd64339cb52838f07d54fef974815f14dc5d7da1
Alex Grintsvayg <alex@flickswitch.cc>
2012-08-10 08:07:49 -0400

    minor fix

--- _plugins/jsminify.rb
+++ _plugins/jsminify.rb
@@ -49,7 +49,7 @@ module Jekyll
       # MinJSFile object.
       def generate(site)
         site.static_files.clone.each do |sf|
-          if sf.kind_of?(Jekyll::StaticFile) && sf.path =~ /\.js$/ && !(sf.path =~ /\.min\./)
+          if sf.kind_of?(Jekyll::StaticFile) && sf.path =~ /\.js$/ && !(sf.path =~ /\.min\.js$/)
             site.static_files.delete(sf)
             name = File.basename(sf.path)
             destination = File.dirname(sf.path).sub(site.source, '')
